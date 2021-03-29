b62d290520c650404ab2dbf7a8e94d45d99f02ae
wjdtngh950 <wjdtngh950@naver.com>
2021-03-29 18:45:46 +0900

    Create for3_550.c

---
+++ for3_550.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void){
+    int n;
+
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int k =0;k<n;k++){
+        for(int g=0;g<=k;g++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
