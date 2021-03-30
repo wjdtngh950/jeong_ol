bb4f7bf804cc3765085b60c38bd1697a93ca1c55
wjdtngh950 <wjdtngh950@naver.com>
2021-03-30 18:10:17 +0900

    Update for3_551-2.c

--- for3_551-2.c
+++ for3_551-2.c
@@ -3,14 +3,15 @@ int main(){
     int n;
     scanf("%d", &n);
 
-    for(int i = 0; i<n; i++){
-        for(int j = n;j>i;j--){
-            printf("*");
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            if(i>j){
+                printf(" ");
+            }
+            else
+                printf("*");
         }
         printf("\n");
-        for(int k =0; k<=i;k++){
-            printf(" ");
-        }
     }
     return 0;
 }
