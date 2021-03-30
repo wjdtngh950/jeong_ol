b6a600336fe8f311f39257f361a7573de6ad7a6b
wjdtngh950 <wjdtngh950@naver.com>
2021-03-30 18:56:32 +0900

    Update for3_552.c

--- for3_552.c
+++ for3_552.c
@@ -4,14 +4,17 @@ int main(){
     scanf("%d", &n);
 
     for(int i = 0;i<n;i++){
-        for(int j = 0;j<n;j++){
-            if(i>j){
-                printf(" ");
-            }
-            else
-            printf("*");
+        for(int j = 0;j<2*n-1-i;j++){
+         if(i>j){
+         printf(" ");
+         }
+         else
+             printf("*");
         }
         printf("\n");
+
     }
     return 0;
+
+
 }
