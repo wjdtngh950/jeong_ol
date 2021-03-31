1861033ba65cc893263d1f5adcb474138661ba74
wjdtngh950 <wjdtngh950@naver.com>
2021-03-31 18:04:10 +0900

    Update for3_147.c

--- for3_147.c
+++ for3_147.c
@@ -8,10 +8,10 @@ int main(){
     for(int i = 0;i<n;i++){
         for(int j =0;j<n;j++){
             if(i<=j){
-            printf("%d", num++);
+            printf("%d ", num++);
         }
             else{
-            printf(" ");
+            printf("  ");
             }
         }
         printf("\n");
