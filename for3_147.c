730a290f608e8c5207b199d787465b821c47b0e8
wjdtngh950 <wjdtngh950@naver.com>
2021-03-31 18:03:36 +0900

    Create for3_147.c

---
+++ for3_147.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int num=1;
+
+    scanf("%d", &n);
+
+    for(int i = 0;i<n;i++){
+        for(int j =0;j<n;j++){
+            if(i<=j){
+            printf("%d", num++);
+        }
+            else{
+            printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
