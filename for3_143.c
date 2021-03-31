ada4226e3257398c3d15b2e98b43a5c26d61b9c1
wjdtngh950 <wjdtngh950@naver.com>
2021-03-31 16:29:23 +0900

    Create for3_143.c

---
+++ for3_143.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for(int i =0;i<n;i++){
+        for(int j = 0; j<2*n-1-i;j++){
+            if(i>j){
+                printf(" ");
+            }
+            else{
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    for(int k=n-2;k>=0;k--){
+        for(int g=0;g<2*n-1-k;g++){
+            if(k>g){
+                printf(" ");
+            }
+            else{
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
