a3ed1bafc85f7cd8f3edb26de7eebe1dabb16033
wjdtngh950 <wjdtngh950@naver.com>
2021-04-02 17:43:11 +0900

    Create arr1_559.c

---
+++ arr1_559.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    double arr[6]={85.6, 79.5, 83.1, 80.0, 78.2, 75.0};
+    int c1;
+    int c2;
+    scanf("%d %d", &c1, &c2);
+    printf("%.1lf", (arr[c1-1])+(arr[c2-1]));
+    return 0;
+}
