2d0bf85cad370601ac5c8d6a3a6d9b79ab8612ca
wjdtngh950 <wjdtngh950@naver.com>
2021-04-05 01:22:06 +0900

    Create arr1_558-2.c

---
+++ arr1_558-2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int target = 0;
+
+    for(int i =0; i<100; i++){
+        scanf(" %d", &arr[i]);
+        if(arr[i]==0){
+            target = i;
+            break;
+        }
+    }
+    for(int i =target-1; i>=0;i--){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
