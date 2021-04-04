51a84b7b7326834f12ab1c351fec05c3c2b69a1b
wjdtngh950 <wjdtngh950@naver.com>
2021-04-05 02:01:15 +0900

    Create arr1_560-2.c

---
+++ arr1_560-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int arr[10];
+
+    for(int i = 0; i<10; i++){
+        scanf("%d", &arr[i]);
+    }
+    int min =arr[0];
+    for(int i =0; i<10; i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("%d", min);
+    return 0;
+}
