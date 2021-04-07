2c68ede2c7bd0ea8e8f681e91f938506aad4e823
wjdtngh950 <wjdtngh950@naver.com>
2021-04-07 23:45:03 +0900

    Create arr1_150.c

---
+++ arr1_150.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char arr[10];
+
+    for(int i=0; i<=9;i++){
+        scanf(" %c", &arr[i]);
+    }
+    for(int i =9;i>=0;i--){
+        printf("%c ", arr[i]);
+    }
+    return 0;
+}
