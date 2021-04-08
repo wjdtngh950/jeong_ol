210fa108e063da8dc2faa288bab32e1cd1896f43
wjdtngh950 <wjdtngh950@naver.com>
2021-04-08 16:37:35 +0900

    Create arr1_563-4.c

---
+++ arr1_563-4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int target = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = target;
+            }
+        }
+    }
+    for (int k = 0; k < 10; k++) {
+        printf("%d ", arr[k]);
+    }
+    return 0;
+}
