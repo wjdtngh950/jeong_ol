83bedd0f165143b610ff3bb3802424f0e385c219
wjdtngh950 <wjdtngh950@naver.com>
2021-04-08 19:07:29 +0900

    Update arr1_158.c

--- arr1_158.c
+++ arr1_158.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 int main(){
     int arr[100];
+    int cnt=0;
 
-    for(int i =0; i<100;i++){
+    for(int i =0; i<100;i++) {
         scanf(" %d", &arr[i]);
+        if (arr[i] == 0) {
+            break;
+        }
+        cnt++;
+    }
+    printf("%d\n", cnt);
+    for(int i=0; i<100;i++){
         if(arr[i]==0){
             break;
         }
