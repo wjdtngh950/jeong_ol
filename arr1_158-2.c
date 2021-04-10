66785dc7ea69cc696356ea6785f5d527e84e4ada
wjdtngh950 <wjdtngh950@naver.com>
2021-04-10 21:14:40 +0900

    Create arr1_158-2.c

---
+++ arr1_158-2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int odd=0;
+    int even=0;
+    int cnt=0;
+
+    for(int i =0; i<100; i++){
+        scanf("%d", &arr[i]);
+        if(arr[i]==0){
+            break;
+        }
+        cnt++;
+    }
+    printf("%d\n", cnt);
+    for(int i =0; i<cnt;i++){
+        if(arr[i]%2==0){
+            printf("%d ", arr[i]/2);
+        }
+        else{
+            printf("%d ", arr[i]*2);
+        }
+    }
+    return 0;
+}
