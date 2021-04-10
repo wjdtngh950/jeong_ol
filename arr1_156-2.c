916b7bfd174d813447eaa4165f5bf4491f9e5c63
wjdtngh950 <wjdtngh950@naver.com>
2021-04-10 20:09:30 +0900

    Create arr1_156-2.c

---
+++ arr1_156-2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int min_val=1;
+    int max_val=0;
+
+    for(int i =0;i<100;i++){
+        scanf("%d", &arr[i]);
+        if(arr[i]==999){
+            break;
+        }
+        if(min_val>arr[i]){
+            min_val=arr[i];
+        }
+        if(max_val<arr[i]){
+            max_val=arr[i];
+        }
+    }
+    printf("max : %d\nmin : %d", max_val, min_val);
+    return 0;
+}
