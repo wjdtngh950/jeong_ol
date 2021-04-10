0d78e36569b7c7a71737f8394da788709244bad8
wjdtngh950 <wjdtngh950@naver.com>
2021-04-10 20:01:50 +0900

    Create arr1_154-2.c

---
+++ arr1_154-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    float arr[6];
+    float sum = 0;
+
+    for (int i = 0; i < 6; i++) {
+        scanf(" %f", &arr[i]);
+        sum += arr[i];
+    }
+    printf("%.1f", sum / 6);
+    return 0;
+}
