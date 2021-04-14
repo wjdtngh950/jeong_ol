8f2183224fa86e753cae6cb9d6001e2801762c1e
wjdtngh950 <wjdtngh950@naver.com>
2021-04-14 17:59:56 +0900

    Create arr1_159-44.c

---
+++ arr1_159-44.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int n;
+    scanf("%d", &n);
+
+    for(int i =0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i =0;i<n;i++){
+        for(int j =0; j<n-1;j++){
+            if(arr[j]<arr[j+1]){
+                int target = arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=target;
+            }
+        }
+    }
+    for(int i=0; i<n;i++){
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
