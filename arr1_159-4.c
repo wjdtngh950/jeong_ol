5ff9acc87f3450ce4e493aa182400e8d08e31ee0
wjdtngh950 <wjdtngh950@naver.com>
2021-04-14 17:56:11 +0900

    Create arr1_159-4.c

---
+++ arr1_159-4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int n;
+    scanf("%d", &n);
+
+    for(int i =0;i<n;i++){
+        scanf("%d", arr[i]);
+    }
+    for(int i =0;i<n;i++){
+        int i
+    }
+}
