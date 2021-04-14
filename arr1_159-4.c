3a621d521a1c1d297ddb4b45fe3a10e9e64eebb5
wjdtngh950 <wjdtngh950@naver.com>
2021-04-14 18:00:14 +0900

    Update arr1_159-4.c

--- arr1_159-4.c
+++ arr1_159-4.c
@@ -5,9 +5,19 @@ int main(){
     scanf("%d", &n);
 
     for(int i =0;i<n;i++){
-        scanf("%d", arr[i]);
+        scanf("%d", &arr[i]);
     }
     for(int i =0;i<n;i++){
-        int i
+        for(int j =0; j<n-1;j++){
+            if(arr[j]<arr[j+1]){
+                int target = arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=target;
+            }
+        }
     }
+    for(int i=0; i<n;i++){
+        printf("%d\n", arr[i]);
+    }
+    return 0;
 }
