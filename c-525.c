4c6ba31926c92bc46537df837f5a3d4fbd7bc4e9
wjdtngh950 <wjdtngh950@naver.com>
2021-04-15 18:54:06 +0900

    Create c-525.c

---
+++ c-525.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n1;
+    int n2;
+    int n3;
+
+    scanf("%d %d %d", &n1, &n2, &n3);
+        printf("%d %d", n1>n2&&n3, n1==n2&&n3);
+        return 0;
+}
