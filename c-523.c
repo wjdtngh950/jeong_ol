23a52be2d018a2c10059b83da3f0b299efb1e593
wjdtngh950 <wjdtngh950@naver.com>
2021-04-15 18:46:52 +0900

    Update c-523.c

--- c-523.c
+++ c-523.c
@@ -1,29 +1,11 @@
 #include<stdio.h>
 int main(){
-    int n1;
-    int n2;
+    int n1,n2;
+    scanf("%d %d",&n1,&n2);
+    printf("%d > %d --- %d\n",n1,n2,n1>n2);
+    printf("%d < %d --- %d\n",n1,n2,n1<n2);
+    printf("%d >= %d --- %d\n",n1,n2,n1>=n2);
+    printf("%d <= %d --- %d",n1,n2,n1<=n2);
 
-    scanf("%d %d", &n1, &n2);
-
-    if(n1<n2){
-        printf("%d > %d --- 0\n", n1, n2);
-    }
-    if(n1<n2){
-        printf("%d < %d --- 1\n", n1, n2);
-    }if(n1<=n2){
-        printf("%d >= %d --- 0\n", n1, n2);
-    }if(n1<=n2){
-        printf("%d <= %d --- 1", n1, n2);
-    }
-    if(n1>n2){
-        printf("%d > %d --- 1\n", n1, n2);
-    }
-    if(n1>n2){
-        printf("%d < %d --- 0\n", n1, n2);
-    }if(n1>=n2){
-        printf("%d >= %d --- 1\n", n1, n2);
-    }if(n1>=n2){
-        printf("%d <= %d --- 0", n1, n2);
-    }
     return 0;
 }
