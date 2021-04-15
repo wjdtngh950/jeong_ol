dfbdbcad37b5b97f7836a7b56bbf8da748a86fbe
wjdtngh950 <wjdtngh950@naver.com>
2021-04-15 18:03:23 +0900

    Create c-521.c

---
+++ c-521.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n1;
+    int n2;
+
+    scanf("%d %d", &n1,&n2);
+    n1++;
+    --n2;
+    printf("%d %d ", n1, n2);
+    printf("%d", --n1*n2);
+    return 0;
+}
