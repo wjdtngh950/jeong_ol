5a3e3bddf92a2d6054283b6ab183a948df188ac3
wjdtngh950 <wjdtngh950@naver.com>
2021-04-15 19:20:47 +0900

    Create c-111.c

---
+++ c-111.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n1;
+    int n2;
+    int n3;
+    int n4;
+    int sum;
+
+    scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
+    sum=n1+n2+n3+n4;
+printf("sum %d\navg %d", sum, sum/4);
+return 0;
+
+}
