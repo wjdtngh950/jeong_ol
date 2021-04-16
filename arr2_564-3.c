da9c97ff08c62ff1e136d0fc26ca4efd625c6fc6
wjdtngh950 <wjdtngh950@naver.com>
2021-04-17 02:54:34 +0900

    Create arr2_564-3.c

---
+++ arr2_564-3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    char arr[100];
+    char c;
+    int cnt[26]={0, };
+
+    for(int i=0;i<100;i++){
+        scanf(" %c", &c);
+        if(!('A'<=c&&c<='Z')){
+            break;
+        }
+        cnt[c-'A']++;
+    }
+    for(int i=0;i<26;i++){
+        if(cnt[i]!=0){
+            printf("%c : %d\n", i+65, cnt[i]);
+        }
+    }
+    return 0;
+}
