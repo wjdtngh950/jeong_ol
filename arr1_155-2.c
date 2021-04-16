0dc45a7b44162871e91d2324b6d0fabf77cfecf8
wjdtngh950 <wjdtngh950@naver.com>
2021-04-17 03:26:14 +0900

    Create arr1_155-2.c

---
+++ arr1_155-2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    char arr[6]={'J', 'U', 'N', 'G', 'O', 'L'};
+    char c;
+    int search =-1;
+
+    scanf(" %c", &c);
+    for(int i=0;i<6;i++){
+        if(c==arr[i]){
+            search=i;
+            break;
+        }
+    }
+    if(search==-1){
+        printf("none");
+    }
+    else{
+        printf("%d", search);
+    }
+    return 0;
+}
