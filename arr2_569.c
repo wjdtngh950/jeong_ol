cc21e6573aef9a986ef5cea2d05ba690bc96fe7c
wjdtngh950 <wjdtngh950@naver.com>
2021-04-18 23:06:35 +0900

    Create arr2_569.c

---
+++ arr2_569.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int arr[5][4];
+    int cnt =0;
+
+    for(int i =0;i<5;i++){
+        for(int j =0; j<4;j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i =0; i<5;i++){
+        int sum =0;
+        for(int j=0; j<4;j++){
+            sum+=arr[i][j];
+        }
+        if((double)sum/4>=80){
+            printf("pass\n");
+            cnt ++;
+        }
+        else{
+            printf("fail\n");
+        }
+    }
+    printf("Successful : %d", cnt);
+    return 0;
+}
