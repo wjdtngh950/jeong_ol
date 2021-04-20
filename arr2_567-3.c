48a3e6b675cb4ce8ffb09dc1dfbd5e2ad17d2f3e
wjdtngh950 <wjdtngh950@naver.com>
2021-04-20 17:53:53 +0900

    Create arr2_567-3.c

---
+++ arr2_567-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int arr[3][5]={
+            {5, 8, 10, 6, 4},
+            {11, 20, 1, 13, 2},
+            {7, 9, 14, 22, 3}
+    };
+
+    for(int i=0; i<3;i++){
+        for(int j=0; j<5;j++){
+            printf("%2d   ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
