337a13a2dd80ee89862842c64adcfa300a4dbf98
wjdtngh950 <wjdtngh950@naver.com>
2021-04-21 18:01:23 +0900

    Create arr2_169.c

---
+++ arr2_169.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char arr[3][5];
+
+    for(int i =0; i<3;i++){
+        for(int j =0; j<5;j++){
+            scanf(" %c", &arr[i][j]);
+        }
+    }
+    for(int i=0; i<3;i++){
+        for(int j =0; j<5;j++){
+            printf("%c ", (arr[i][j]+32));
+        }
+        printf("\n");
+    }
+    return 0;
+}
