4a04f5bf1c1d4fffa3181a6dd0d9ce5c9ea2dea7
wjdtngh950 <wjdtngh950@naver.com>
2021-04-21 16:46:42 +0900

    Create arr2_160-2.c

---
+++ arr2_160-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int arr[10];
+    int n;
+    int cnt[6]={0, };
+
+    for(int i =1; i<=10;i++){
+        scanf("%d", &n);
+        cnt[n-1]++;
+    }
+
+    for(int i =1; i<=6;i++){
+        printf("%d : %d\n", i, cnt[i-1]);
+    }
+    return 0;
+}
