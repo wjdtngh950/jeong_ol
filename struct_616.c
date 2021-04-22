ff399937c237cd17410b9fa7990a32759deb35f6
wjdtngh950 <wjdtngh950@naver.com>
2021-04-22 18:35:34 +0900

    Update struct_616.c

--- struct_616.c
+++ struct_616.c
@@ -10,6 +10,6 @@ int main(){
     tra z;
 
     scanf("%d %d %d %d %d %d", &x.x, &x.y, &y.x, &y.y, &z.x, &z.y);
-    printf("(%.1lf, %.1lf)", ((double)(x.x+y.x+z.x)/3, (double)(x.y+y.y+z.y)/3);
+    printf("(%.1lf, %.1lf)", (double)(x.x+y.x+z.x)/3, (double)(x.y+y.y+z.y)/3);
     return 0;
 }
