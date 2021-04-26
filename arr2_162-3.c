9aec0fae17ed83b4f0941497db89f2b678c6a8a4
wjdtngh950 <wjdtngh950@naver.com>
2021-04-26 19:22:21 +0900

    Create arr2_162-3.c

---
+++ arr2_162-3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int arr[10];
+    arr[0];
+    arr[1];
+
+    scanf("%d %d", &arr[0], &arr[1]);
+    printf("%d %d ", arr[0], arr[1]);
+
+    for(int i=2;i<10;i++){
+        arr[i]=arr[i-2]+arr[i-1];
+        printf("%d ", arr[i]%10);
+    }
+    return 0;
+}
