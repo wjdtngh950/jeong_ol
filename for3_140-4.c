9be77349a52a159c87926a54f62ebfcea90511c5
wjdtngh950 <wjdtngh950@naver.com>
2021-05-03 15:31:38 +0900

    Create for3_140-4.c

---
+++ for3_140-4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main(){
+    int n;
+    int sum =0;
+    int cnt=0;
+
+    for(int i =0;i<20;i++){
+        scanf("%d", &n);
+        if(n==0){
+            break;
+        }
+        sum+=n;
+        cnt++;
+    }
+    printf("%d %d", sum, sum/cnt);
+    return 0;
+}
