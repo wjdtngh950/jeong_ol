7711315fe21bad47a89c38b09fd18ce22f21b09c
wjdtngh950 <wjdtngh950@naver.com>
2021-05-03 15:44:48 +0900

    Create for3_142-4.c

---
+++ for3_142-4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int n;
+
+    scanf("%d", &n);
+
+    for(int i =0; i<n;i++){
+        for(int j =0;j<n;j++){
+            if(i>=j){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j =n-1; j>0;j--){
+            if(i<j){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
