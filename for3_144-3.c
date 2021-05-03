39a6a08efb03cb767c69022f4c761978ec857038
wjdtngh950 <wjdtngh950@naver.com>
2021-05-03 16:33:02 +0900

    Create for3_144-3.c

---
+++ for3_144-3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for(int i =2*n-1;i>0;i-=2){
+        for(int j=1;j<=2*n-1;j++){
+            if(i<=j){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
