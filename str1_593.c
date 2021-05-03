078a8d70d9a6fbc7a7ed2360a9fb61d6e4b29d1a
wjdtngh950 <wjdtngh950@naver.com>
2021-05-03 22:40:43 +0900

    Create str1_593.c

---
+++ str1_593.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int n;
+
+for(;;){
+    printf("ASCII code =? ");
+    scanf("%d", &n);
+    if(33<=n&&n<=127) {
+        printf("%c\n", n);
+    }
+    else{
+        break;
+    }
+}
+return 0;
+}
