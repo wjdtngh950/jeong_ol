b67ab184053efc5b6a49a9f50c799b28bf77eedc
wjdtngh950 <wjdtngh950@naver.com>
2021-05-06 19:08:17 +0900

    Create str1_599-2.c

---
+++ str1_599-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char c[100];
+    int len;
+
+    scanf("%s", c);
+    len=strlen(c);
+
+    for(int i =0;i<len;i++){
+        if('A'<=c[i]&&c[i]<='Z'){
+            printf("%c", c[i]);
+        }
+        else if('a'<=c[i]&&c[i]<='z'){
+            printf("%c", c[i]-32);
+        }
+    }
+    return 0;
+}
