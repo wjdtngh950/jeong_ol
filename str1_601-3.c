fcb8af1480b17b17904aeed5aaa80212712ec825
wjdtngh950 <wjdtngh950@naver.com>
2021-05-10 17:25:43 +0900

    Create str1_601-3.c

---
+++ str1_601-3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[101];
+    int len;
+
+    scanf("%s", str);
+    len=strlen(str);
+
+    for(int i =1;i<=len;i++){
+        for(int j=0;j<len;j++){
+            printf("%c", str[(len-i+j)%len]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
