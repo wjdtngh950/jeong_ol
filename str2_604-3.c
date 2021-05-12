754bf3abea482a11207cdfff27fe6005989c832b
wjdtngh950 <wjdtngh950@naver.com>
2021-05-12 23:07:53 +0900

    Create str2_604-3.c

---
+++ str2_604-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[10][21];
+    char last;
+    int len;
+
+    for(int i=0; i<10;i++){
+        scanf("%s", str[i]);
+    }
+    scanf(" %c", &last);
+    for(int i=0; i<10;i++){
+        len=strlen(str[i]);
+        if(str[i][len-1]==last){
+            printf("%s\n", str[i]);
+        }
+    }
+    return 0;
+}
