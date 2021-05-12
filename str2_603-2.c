b48398ba4111e1543d80b042576356cf975521b7
wjdtngh950 <wjdtngh950@naver.com>
2021-05-12 19:38:59 +0900

    Create str2_603-2.c

---
+++ str2_603-2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char str[101];
+    int len;
+    int cnt;
+    gets(str);
+    len = strlen(str);
+    if (str[0] == ' ') {
+        cnt = 0;
+    }
+    else{
+        cnt=1;
+    printf("%c", str[0]);
+    }
+    for(int i =1;i<len;i++){
+        if(str[i]==' '&&str[i+1]!=' '){
+            cnt++;
+            if(cnt%2==1) {
+                printf("\n");
+            }
+        }
+        if(str[i]!=' '&&cnt%2==1){
+            printf("%c", str[i]);
+        }
+    }
+    return 0;
+}
