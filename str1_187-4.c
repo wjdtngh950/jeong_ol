a106d64ea6038d52699eb12ded846f65fb80689a
wjdtngh950 <wjdtngh950@naver.com>
2021-05-12 23:54:13 +0900

    Create str1_187-4.c

---
+++ str1_187-4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str1[101];
+    char str2[101];
+    int n;
+    int len;
+    scanf("%s", str1);
+    while((len=strlen(str1))>1){
+        strcpy(str2, str1);
+        scanf("%d", &n);
+        if(n>len){
+            n=len;
+        }
+        strcpy(str1+n-1, str2+n);
+        printf("%s\n", str1);
+    }
+    return 0;
+}
