92aa1c605f0b89d3a684d319247c3f8fc6e2674e
wjdtngh950 <wjdtngh950@naver.com>
2021-05-17 19:16:24 +0900

    Create str2_602-4.c

---
+++ str2_602-4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[5][31];
+
+    for(int i=0; i<5;i++){
+        scanf("%s", str[i]);
+    }
+
+    for(int i=4;i>=0;i--){
+        printf("%s\n", str[i]);
+    }
+    return 0;
+}
