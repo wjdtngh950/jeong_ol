90d95038b1f9d87bd3a2144534ca1c21c3be486a
wjdtngh950 <wjdtngh950@naver.com>
2021-05-17 23:53:44 +0900

    Update str2_194.c

--- str2_194.c
+++ str2_194.c
@@ -7,10 +7,11 @@ int n;
 int len;
 
 scanf("%s %s %d", str1, str2, &n);
-
+len=strlen(str2);
 strcat(str1, str2);
 printf("%s\n", str1);
 strncpy(str2, str1, n);
+str2[len<n?n:len]='\0';
 printf("%s", str2);
 return 0;
 }
