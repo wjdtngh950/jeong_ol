cd3ea8e0e651b6b4991d9588b4d3258226ead0f6
wjdtngh950 <wjdtngh950@naver.com>
2021-05-17 21:29:34 +0900

    Create str2_216.c

---
+++ str2_216.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str[21];
+    int len;
+
+    for (;;) {
+       scanf("%s", str);
+       printf("\n");
+       if(str=="END"){
+           break;
+       }
+       len=strlen(str);
+       for(int i=len-1;i>=0;i--){
+           printf("%c", str[i]);
+       }
+    }
+    return 0;
+}
