061b2f4125823ba4ae3deae2397364634933a3cf
wjdtngh950 <wjdtngh950@naver.com>
2021-05-17 19:52:56 +0900

    Create str2_604-5.c

---
+++ str2_604-5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[10][21];
+    int n;
+    int len;
+    char last;
+
+    for(int i=0;i<10;i++){
+        scanf("%s", str[i]);
+    }
+    scanf(" %c", &last);
+    for(int i =0; i<10;i++){
+        len=strlen(str[i]);
+        if(str[i][len-1]==last){
+            printf("%s\n", str[i]);
+        }
+    }
+    return 0;
+}
