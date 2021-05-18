3605dabd84220135ed30f17b5aad6aec21ff9146
wjdtngh950 <wjdtngh950@naver.com>
2021-05-18 16:27:52 +0900

    Update str2_608-3.c

--- str2_608-3.c
+++ str2_608-3.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
 #include<string.h>
+int main(){
+    char str[101];
+
+    scanf("%s", str);
+    if(strstr(str, "c")){
+        printf("Yes ");
+    }
+    else{
+        printf("No ");
+    }
+    if(strstr(str, "ab")){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
