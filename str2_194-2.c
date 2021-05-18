5365008d641c32d9e4c1341329fd9d737316cdfc
wjdtngh950 <wjdtngh950@naver.com>
2021-05-18 17:16:58 +0900

    Create str2_194-2.c

---
+++ str2_194-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str1[101];
+    char str2[101];
+    int n;
+    int len;
+
+    scanf("%s %s %d", str1, str2, &n);
+    len=strlen(str2);
+    strcat(str1, str2);
+    printf("%s\n", str1);
+    strncpy(str2, str1, n);
+    str2[len<n?n:len]='\0';
+    printf("%s", str2);
+}
