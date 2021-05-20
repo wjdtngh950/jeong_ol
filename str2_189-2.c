bb3331f5bf1d0910aa5ea60e4e939c7ebfa10a43
wjdtngh950 <wjdtngh950@naver.com>
2021-05-20 22:22:16 +0900

    Create str2_189-2.c

---
+++ str2_189-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char str[10000];
+    char* str_stk[101];
+    int top = -1;
+
+    gets(str);
+   str_stk[++top]=strtok(str, " ");
+
+   while(str_stk[top]!=NULL){
+       str_stk[++top]=strtok(NULL, " ");
+   }
+   while(top>0){
+       printf("%s\n", str_stk[--top]);
+   }
+   return 0;
+}
