c2e005f0bd531e73e70767bbb38d3eb03b3c9e8f
wjdtngh950 <wjdtngh950@naver.com>
2021-05-20 21:43:45 +0900

    Update str2_609.c

--- str2_609.c
+++ str2_609.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<string.h>
-int main(){
+
+int main() {
     char str[3][1000];
-    int min=0;
+    int min = 0;
 
     scanf("%s %s %s", str[0], str[1], str[2]);
-    min=(strcmp(str[min], str[1])>0?1:min);
-    min=(strcmp(str[min], str[2])>0?2:min);
+    min = (strcmp(str[min], str[1]) > 0 ? 1 : min);
+    min = (strcmp(str[min], str[2]) > 0 ? 2 : min);
     printf("%s", str[min]);
     return 0;
 }
