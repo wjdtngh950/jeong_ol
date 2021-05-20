d1ab5b344061cdaabc5bd0e111f85136e7609b16
wjdtngh950 <wjdtngh950@naver.com>
2021-05-20 22:14:45 +0900

    Update str2_612-2.c

--- str2_612-2.c
+++ str2_612-2.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char str[10000];
+    char str[10000]={0, };
     int n[5];
     int len=0;
-
-    for(int i =0;i<5;i++){
+    for(int i =0; i<5;i++) {
         scanf("%d", &n[i]);
-        sprintf(str+len, "%d", n[i]);
+        sprintf(str+len,"%d", n[i]);
         len=strlen(str);
     }
-    for(int i=0;i<len;i++){
+    for(int i =0;i<len;i++){
         printf("%c", str[i]);
         if(i%3==2){
             printf("\n");
