bbb5ac7b37601bd5f21d5ea30d20a1f3a916be2d
wjdtngh950 <wjdtngh950@naver.com>
2021-05-20 23:10:53 +0900

    Create str2_191-4.c

---
+++ str2_191-4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50][101];
+    int cnt=0;
+
+    for(int i =0; i<50;i++){
+        scanf("%s", str[i]);
+        if(strcmp(str[i], "0")==0){
+            break;
+        }
+        cnt++;
+    }
+    printf("%d\n",cnt);
+    for(int i =0; i<cnt;i++){
+        if(i%2==0){
+            printf("%s\n", str[i]);
+        }
+    }
+    return 0;
+}
