45c3561a89dffe88994a01570be9f92a505d8f91
wjdtngh950 <wjdtngh950@naver.com>
2021-11-18 22:13:38 +0900

    Update arr1_563-4오름.c

--- arr1_563-4오름.c
+++ arr1_563-4오름.c
@@ -8,7 +8,7 @@ int main() {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 9; j++) {
             if (arr[j] > arr[j + 1]) {
-                int target = arr[j];
+                int target  arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = target;
             }
